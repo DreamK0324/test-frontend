83ae4382c21fb6d50570949bf712eea49ebced99
Dream K <dreamk@ZoneBook.local>
2023-07-14 20:51:33 -0400

    fix app.js element related problem

--- src/components/containers/UserContainer.js
+++ src/components/containers/UserContainer.js
@@ -1,132 +1,32 @@
-// import React, { Component } from "react";
-// import { connect } from "react-redux";
-// import { 
-//     fetchUserThunk,
-//     fetchAllMoviesThunk,
-//     editMovieThunk 
-// } from "../../store/thunks";
-
-// import { UserView } from "../views";
-
-// class UserContainer extends Component {
-//     componentDidMount() {
-//         //getting user ID from url
-//         this.props.fetchUser(this.props.match.params.id);
-//         this.props.fetchMovies();
-//     }
-
-//     render() {
-//         return (
-//             <UserView 
-//                 user={this.props.user}
-//                 editMovie={this.props.editMovie}
-//                 allMovies={this.props.allMovies}
-//             />
-//         );
-//     }
-// }
-
-// // map state to props
-// const mapState = (state) => {
-//     return {
-//         user: state.user,
-//         // allMovies: state.allMovies,
-
-//     };
-// };
-
-// // map dispatch to props
-// const mapDispatch = (dispatch) => {
-//     return {
-//         fetchUser: (id) => dispatch(fetchUserThunk(id)),
-//         // editMovie: (movie) => dispatch(editMovieThunk(movie)),
-//         // fetchMovies: () => dispatch(fetchAllMoviesThunk()),
-
-//     };
-// };
-
-// export default connect(mapState, mapDispatch)(UserContainer);
-
-//////////////////////////////////////////////////////////////////////////////////////
-
-// import React, { useEffect } from 'react';
-// import { connect } from 'react-redux';
-
-// import { fetchUserThunk } from '../../store/thunks';
-
-// import UserView from '../views/UserView';
-
-// const UserContainer = ({ userId, fetchUser, user }) => {
-//   useEffect(() => {
-//     fetchUser(userId);
-//   }, [fetchUser, userId]);
-
-//   return (
-//     <div>
-//       <UserView 
-//         user={user} 
-//         />
-//     </div>
-//   );
-// };
-
-// const mapStateToProps = (state, ownProps) => {
-//   const { match } = ownProps;
-//   const userId = match?.params?.id;
-//   return {
-//     userId,
-//     user: state.user,
-//   };
-// };
-  
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     fetchUser: (userId) => dispatch(fetchUserThunk(userId)),
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
 import React, { useEffect } from "react";
-import { connect } from 'react-redux';
-import { fetchUserThunk, deleteUserThunk } from "../../store/thunks";
-import UserView from "../views/UserView";
+import { connect } from "react-redux";
+import { useParams } from 'react-router-dom';
+import { fetchUserThunk } from "../../store/thunks";
+import { UserView } from "../views";
 
-const UserContainer = ({ userId, fetchUser, user, deleteUser }) => {
+const UserContainer = ({ fetchUser, user }) => {
+  const { id } = useParams();
+
   useEffect(() => {
-    fetchUser(userId);
-  }, [fetchUser, userId]);
+    fetchUser(id);
+  }, [fetchUser, id]);
 
   return (
-    <div>
-      <UserView user={user} deleteUser={deleteUser} />
-    </div>
+    <UserView user={user} />
   );
 };
 
-// Map state to props
-const mapState = (state, ownProps) => {
-  const { match } = ownProps;
-  const userId = match?.params?.id;
-  console.log('State:', state); // Check the entire state object
-  console.log('User:', state.user); // Check the user property specifically
-  console.log('User ID:', userId); // Check the extracted user ID
+// map state to props
+const mapState = (state) => {
   return {
-    userId,
     user: state.user,
   };
 };
 
-
-// Map dispatch to props
+// map dispatch to props
 const mapDispatch = (dispatch) => {
   return {
-    fetchUser: (userId) => dispatch(fetchUserThunk(userId)),
-    deleteUser: (userId) => dispatch(deleteUserThunk(userId)),
+    fetchUser: (id) => dispatch(fetchUserThunk(id)),
   };
 };
 
